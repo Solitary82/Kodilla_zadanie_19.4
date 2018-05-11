7b90a9a36a0873d58281058eccb1674b92117077
Marta Waplak <mwaplak@gmail.com>
2018-05-11 21:05:32 +0200

    Fix formatting in comment.js and install Babel with plugins

--- comments.js
+++ comments.js
@@ -8,41 +8,42 @@ function comments (state = [], action) {
     switch(action.type) {
         case ADD_COMMENT:
             return [{
-                     id: action.id,
-                     text: action.text,
-                     votes: 0
+                id: action.id,
+                text: action.text,
+                votes: 0
             }
-                    , ...state.comments];
+            , ...state.comments];
 
         case REMOVE_COMMENT:
             return state.map(comment => comment.id !== action.id);
             
         case EDIT_COMMENT:
-                return state.map(comment => {
-                    if(comment.id === action.id) 
-                        return {...comment,
-                            text: action.text
-                        }
-                        return comment;
-                    });
+            return state.map(comment => {
+                if(comment.id === action.id) 
+                    return {...comment,
+                        text: action.text
+                    }
+                    return comment;
+            });
             
         case THUMB_UP_COMMENT:
             return state.map(comment => {
-                    if(comment.id === action.id)  
-                        return {...comment, votes: comment.votes + 1
-                        }
+                if(comment.id === action.id)  
+                    return {...comment, votes: comment.votes + 1
+                    }
                     return comment;
-                    });
+            });
             
         case THUMB_DOWN_COMMENT:
             return state.map(comment => {
-                    if(comment.id === action.id)  
-                        return {...comment, votes: comment.votes + 1
-                        }
-                    return comment
-                    });
+                if(comment.id === action.id)  
+                    return {...comment, votes: comment.votes + 1
+                    }
+                return comment
+            });
         default:
-        return state;
+            return state;
     }
+};
                     
 export default comments;
